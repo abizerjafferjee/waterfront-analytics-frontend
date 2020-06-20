d59023943105b444d8ab9bff0e41712ca17a8455
abizerjafferjee <abizerjafferjee@gmail.com>
2020-06-20 14:27:03 -0400

    adding the build folder

---
+++ build/precache-manifest.c3fb07369cc5a7149ecfb07103db9fd6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f3f42ff78eb4656f3328d3663edfd84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "079b049c91bdd07362b4",
+    "url": "/static/css/main.c77ae3a4.chunk.css"
+  },
+  {
+    "revision": "998a6be06eaa33e7a591",
+    "url": "/static/js/2.1d828d70.chunk.js"
+  },
+  {
+    "revision": "9d36fd18d7d7b7370f96c95702a6c2fd",
+    "url": "/static/js/2.1d828d70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "079b049c91bdd07362b4",
+    "url": "/static/js/main.1e495eee.chunk.js"
+  },
+  {
+    "revision": "f22505fda0b7a521c3b1",
+    "url": "/static/js/runtime-main.cd1be28d.js"
+  },
+  {
+    "revision": "01ab499968637f34d823e68745c2639b",
+    "url": "/static/media/abizer.01ab4999.jpg"
+  },
+  {
+    "revision": "44be0720efb519706a8db03d5174c3df",
+    "url": "/static/media/data-model.44be0720.svg"
+  },
+  {
+    "revision": "bb08254ecee431f5e28095b6dcedad22",
+    "url": "/static/media/data-science.bb08254e.svg"
+  },
+  {
+    "revision": "8029190eff2129fd43c5a490cafe1d8b",
+    "url": "/static/media/database.8029190e.svg"
+  },
+  {
+    "revision": "ac1714cd810cd6bfafac15a1cdb1e2d1",
+    "url": "/static/media/event-tracking.ac1714cd.svg"
+  },
+  {
+    "revision": "25061a8b4a8812766911702583c9a608",
+    "url": "/static/media/kpi-measurements.25061a8b.svg"
+  },
+  {
+    "revision": "fcd507fe14810a0ad6d8bb9af12d2a83",
+    "url": "/static/media/octo-analytics-logo-white.fcd507fe.png"
+  },
+  {
+    "revision": "4832aea521ac2843e085a6faf3bdce63",
+    "url": "/static/media/strategy.4832aea5.svg"
+  }
+]);
